d8880cc53da79a9cc130b149670caf86de8ad405
jhhwan <junghwanhwang@hansol.com>
2023-03-02 10:02:16 +0900

    RpaVO 파일 추가 2022-03-02

---
+++ src/main/java/com/logislink/order/vo/RpaVO.java
@@ -0,0 +1,294 @@
+package com.logislink.order.vo;
+
+public class RpaVO {
+    private String order_id;
+    private String orderStateName;
+    private String sDateDay;
+    private String sDateTime;
+    private String eDateDay;
+    private String eDateTime;
+    private String chargeType;
+    private String orderCarName;
+    private String orderCarTonName;
+    private String link_stat_03;
+    private String link_stat_21;
+    private String link_stat_18;
+    private String driverName;
+    private String driverTel;
+    private String carNum;
+
+    private String link_charge_03;
+    private String link_charge_21;
+    private String link_charge_18;
+    private int number;
+    private String goodsName;
+    private String goodsWeight;
+    private String custName;
+    private String regdate;
+    private String job_stat_03;
+    private String job_stat_21;
+    private String job_stat_18;
+    private String link_id_03;
+    private String link_id_21;
+    private String link_id_18;
+    private String rpa_msg_03;
+    private String rpa_msg_21;
+    private String rpa_msg_18;
+
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getOrderStateName() {
+        return orderStateName;
+    }
+
+    public void setOrderStateName(String orderStateName) {
+        this.orderStateName = orderStateName;
+    }
+
+    public String getsDateDay() {
+        return sDateDay;
+    }
+
+    public void setsDateDay(String sDateDay) {
+        this.sDateDay = sDateDay;
+    }
+
+    public String getsDateTime() {
+        return sDateTime;
+    }
+
+    public void setsDateTime(String sDateTime) {
+        this.sDateTime = sDateTime;
+    }
+
+    public String geteDateDay() {
+        return eDateDay;
+    }
+
+    public void seteDateDay(String eDateDay) {
+        this.eDateDay = eDateDay;
+    }
+
+    public String geteDateTime() {
+        return eDateTime;
+    }
+
+    public void seteDateTime(String eDateTime) {
+        this.eDateTime = eDateTime;
+    }
+
+    public String getChargeType() {
+        return chargeType;
+    }
+
+    public void setChargeType(String chargeType) {
+        this.chargeType = chargeType;
+    }
+
+    public String getOrderCarName() {
+        return orderCarName;
+    }
+
+    public void setOrderCarName(String orderCarName) {
+        this.orderCarName = orderCarName;
+    }
+
+    public String getOrderCarTonName() {
+        return orderCarTonName;
+    }
+
+    public void setOrderCarTonName(String orderCarTonName) {
+        this.orderCarTonName = orderCarTonName;
+    }
+
+    public String getLink_stat_03() {
+        return link_stat_03;
+    }
+
+    public void setLink_stat_03(String link_stat_03) {
+        this.link_stat_03 = link_stat_03;
+    }
+
+    public String getLink_stat_21() {
+        return link_stat_21;
+    }
+
+    public void setLink_stat_21(String link_stat_21) {
+        this.link_stat_21 = link_stat_21;
+    }
+
+    public String getLink_stat_18() {
+        return link_stat_18;
+    }
+
+    public void setLink_stat_18(String link_stat_18) {
+        this.link_stat_18 = link_stat_18;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getDriverTel() {
+        return driverTel;
+    }
+
+    public void setDriverTel(String driverTel) {
+        this.driverTel = driverTel;
+    }
+
+    public String getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(String carNum) {
+        this.carNum = carNum;
+    }
+
+    public String getLink_charge_03() {
+        return link_charge_03;
+    }
+
+    public void setLink_charge_03(String link_charge_03) {
+        this.link_charge_03 = link_charge_03;
+    }
+
+    public String getLink_charge_21() {
+        return link_charge_21;
+    }
+
+    public void setLink_charge_21(String link_charge_21) {
+        this.link_charge_21 = link_charge_21;
+    }
+
+    public String getLink_charge_18() {
+        return link_charge_18;
+    }
+
+    public void setLink_charge_18(String link_charge_18) {
+        this.link_charge_18 = link_charge_18;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getGoodsWeight() {
+        return goodsWeight;
+    }
+
+    public void setGoodsWeight(String goodsWeight) {
+        this.goodsWeight = goodsWeight;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(String regdate) {
+        this.regdate = regdate;
+    }
+
+    public String getJob_stat_03() {
+        return job_stat_03;
+    }
+
+    public void setJob_stat_03(String job_stat_03) {
+        this.job_stat_03 = job_stat_03;
+    }
+
+    public String getJob_stat_21() {
+        return job_stat_21;
+    }
+
+    public void setJob_stat_21(String job_stat_21) {
+        this.job_stat_21 = job_stat_21;
+    }
+
+    public String getJob_stat_18() {
+        return job_stat_18;
+    }
+
+    public void setJob_stat_18(String job_stat_18) {
+        this.job_stat_18 = job_stat_18;
+    }
+
+    public String getLink_id_03() {
+        return link_id_03;
+    }
+
+    public void setLink_id_03(String link_id_03) {
+        this.link_id_03 = link_id_03;
+    }
+
+    public String getLink_id_21() {
+        return link_id_21;
+    }
+
+    public void setLink_id_21(String link_id_21) {
+        this.link_id_21 = link_id_21;
+    }
+
+    public String getLink_id_18() {
+        return link_id_18;
+    }
+
+    public void setLink_id_18(String link_id_18) {
+        this.link_id_18 = link_id_18;
+    }
+
+    public String getRpa_msg_03() {
+        return rpa_msg_03;
+    }
+
+    public void setRpa_msg_03(String rpa_msg_03) {
+        this.rpa_msg_03 = rpa_msg_03;
+    }
+
+    public String getRpa_msg_21() {
+        return rpa_msg_21;
+    }
+
+    public void setRpa_msg_21(String rpa_msg_21) {
+        this.rpa_msg_21 = rpa_msg_21;
+    }
+
+    public String getRpa_msg_18() {
+        return rpa_msg_18;
+    }
+
+    public void setRpa_msg_18(String rpa_msg_18) {
+        this.rpa_msg_18 = rpa_msg_18;
+    }
+}
